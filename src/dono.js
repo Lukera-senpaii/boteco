e92bce0a8908bc0e2f2b69029aa2c18bf16600ce
Lukera-senpaii <lucasbarbosasilva007@gmail.com>
2021-08-11 12:59:08 -0300

    First commit

---
+++ src/dono.js
@@ -0,0 +1,15 @@
+const dono = (prefix, sender) => {
+  return`
+
+╔══✪〘 DONO 〙✪════
+║
+╠➥NOME: Lucas
+╠➥NICK: Lukera
+╠➥SEXO: MASCULINO
+╠➥NUMERO: wa.me/5516991766933
+╠➥CANAL YT: https://youtu.be/M78QQTEcmYI
+╠➥INSTA: https://www.instagram.com/luke.ra/
+╚═✪〘 Boteco 〙✪══`
+
+}
+exports.dono = dono
